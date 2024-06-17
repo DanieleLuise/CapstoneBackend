224eee1dcfbf028b1873c1a05988ace62036962f
DanieleLuise <luisedaniele03@gmail.com>
2024-06-17 11:12:38 +0200

    commit

---
+++ ProgettoCap/src/main/java/com/example/ProgettoCap/ProgettoCapApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProgettoCap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoCapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoCapApplication.class, args);
+	}
+
+}
---
+++ ProgettoCap/src/test/java/com/example/ProgettoCap/ProgettoCapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProgettoCap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoCapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
